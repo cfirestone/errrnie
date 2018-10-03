7724fb4077a7ef64ceae2f050262b352abeb1769
Chris Murphy <cjmurp2@gmail.com>
2018-10-03 12:53:05 -0500

    real first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../errrnie.zip');
+var kuduApi = 'https://errrnie.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$errrnie';
+var password = 'FglCtZusgL96PlwsBujnqz8z40cm3xiAJS4bKLvLQtTQtB6pt1DLseKaSTew';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('errrnie publish');
+  } else {
+    console.error('failed to publish errrnie', err);
+  }
+});
